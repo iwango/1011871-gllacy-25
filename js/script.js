08a9329c52d2a1c6a356b3d94d1e79123f02ec7c
iwango <41298967+iwango@users.noreply.github.com>
2019-07-07 22:54:32 +0300

    сделаны исправления, по замечаниям проверяющего

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 var btnFeedbackClick = document.querySelector(".btn-feedback");
-var popupFulscreenClick = document.querySelector(".full-screen-field")
+var popupFulscreenClick = document.querySelector(".full-screen-field");
 var popup = document.querySelector(".feedback");
 var popupFulscreen = document.querySelector(".full-screen-field");
 var btnFeedbackClose = document.querySelector(".feedback-close");
+var inputFeedback = document.querySelector(".input-feedback");
 
-
-btnFeedbackClick.addEventListener("click", function () {
+btnFeedbackClick.addEventListener("click", function (evt) {
+  evt.preventDefault();
   popup.classList.add("modal-show");
-  popupFulscreen.classList.add("modal-show")
-  document.getElementById("feedback-name-field").focus();
+  popupFulscreen.classList.add("modal-show");
+  inputFeedback.focus();
 });
 
 popupFulscreenClick.addEventListener("click", function () {
